baf4de6dc57484ede4429d9b77f3766ff3b17223
Luuk Holleman <luukholleman@gmail.com>
2017-10-19 14:07:29 +0200

    Support windows, linux and osx

--- Assets/Editor/Utility.cs
+++ Assets/Editor/Utility.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using UnityEngine;
 namespace ParkitectAssetEditor
 {
     /// <summary>
@@ -12,7 +12,13 @@ namespace ParkitectAssetEditor
         {
             get
             {
-                return string.Format(@"{0}\Parkitect\Mods", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+#if UNITY_STANDALONE_OSX
+				return System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/../Library/Application Support/Parkitect/", "Mods"));
+#elif UNITY_STANDALONE_WIN
+				return System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Parkitect/", "Mods"));
+#else
+				return System.IO.Path.GetFullPath(System.IO.Path.Combine(Application.dataPath + "/..", "Mods"));
+#endif
             }
         } 
     }
